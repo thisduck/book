b6a7463c1fd5f0af27b18eaa848836be7dd92975
Adnan Ali <adnan.ali@gmail.com>
2013-08-24 12:55:58 -0400

    simple one page.

---
+++ app/assets/javascripts/home.js.coffee
@@ -0,0 +1,16 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+
+$("body").noisy
+  intensity:1
+  opacity:.05
+
+$(".header").noisy
+  intensity:1
+  opacity:.05
+
+$(".footer").noisy
+  intensity:1
+  opacity:.05
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '84f29a6608eb7e74ee95fe87a4da8a5941c22888254efb6d86781ae84d60d73a5742df6494c8d122175132ef4cd60a1fb437bda06921ed953a68bdc5ec8b8c8c'
